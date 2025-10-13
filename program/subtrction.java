1857ca42c6f0a37db5faaefbbae02fc63fc05e96
pradeepakt421 <ktpradeepa4@gmail.com>
2025-10-13 10:37:05 +0530

    v2;

---
+++ program/subtrction.java
@@ -0,0 +1,11 @@
+ static void linerSearch(int[] arr,int target){
+        int index=-1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==target){
+                index=i;
+                break;
+            }
+        }
+        if(index==-1) System.out.println("elemnt not found");
+        else System.out.println("elemnr found in "+index);
+    }
